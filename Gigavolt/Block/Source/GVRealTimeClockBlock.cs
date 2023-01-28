30f082d088634ad0488b92c12de48e1915845884
销锋镝铸 <523084467@qq.com>
2023-01-28 23:55:55 +0800

    添加原版实时钟

---
+++ Gigavolt/Block/Source/GVRealTimeClockBlock.cs
@@ -0,0 +1,31 @@
+namespace Game
+{
+    public class GVRealTimeClockBlock : RotateableMountedGVElectricElementBlock
+    {
+        public const int Index = 887;
+
+        public GVRealTimeClockBlock()
+            : base("Models/Gates", "RealTimeClock", 0.5f)
+        {
+        }
+
+        public override GVElectricElement CreateGVElectricElement(SubsystemGVElectricity subsystemGVElectricity, int value, int x, int y, int z)
+        {
+            return new RealTimeClockGVElectricElement(subsystemGVElectricity, new CellFace(x, y, z, GetFace(value)));
+        }
+
+        public override GVElectricConnectorType? GetConnectorType(SubsystemTerrain terrain, int value, int face, int connectorFace, int x, int y, int z)
+        {
+            int data = Terrain.ExtractData(value);
+            if (GetFace(value) == face)
+            {
+                GVElectricConnectorDirection? connectorDirection = SubsystemGVElectricity.GetConnectorDirection(GetFace(value), GetRotation(data), connectorFace);
+                if (connectorDirection == GVElectricConnectorDirection.Top || connectorDirection == GVElectricConnectorDirection.Right || connectorDirection == GVElectricConnectorDirection.Left || connectorDirection == GVElectricConnectorDirection.Bottom || connectorDirection == GVElectricConnectorDirection.In)
+                {
+                    return GVElectricConnectorType.Output;
+                }
+            }
+            return null;
+        }
+    }
+}
---
+++ Gigavolt/Block/Source/RealTimeClockGVElectricElement.cs
@@ -0,0 +1,66 @@
+using Engine;
+
+namespace Game
+{
+    public class RealTimeClockGVElectricElement : RotateableGVElectricElement
+    {
+        public SubsystemTimeOfDay m_subsystemTimeOfDay;
+
+        public uint m_lastClockValue = 0u;
+
+        public RealTimeClockGVElectricElement(SubsystemGVElectricity subsystemGVElectricity, CellFace cellFace)
+            : base(subsystemGVElectricity, cellFace)
+        {
+            m_subsystemTimeOfDay = SubsystemGVElectricity.Project.FindSubsystem<SubsystemTimeOfDay>(throwOnError: true);
+        }
+
+        public override uint GetOutputVoltage(int face)
+        {
+            GVElectricConnectorDirection? connectorDirection = SubsystemGVElectricity.GetConnectorDirection(CellFaces[0].Face, Rotation, face);
+            if (connectorDirection.HasValue)
+            {
+                if (connectorDirection.Value == GVElectricConnectorDirection.Top)
+                {
+                    return GetClockValue() & 0xFu;
+                }
+                if (connectorDirection.Value == GVElectricConnectorDirection.Right)
+                {
+                    return (GetClockValue() >> 4) & 0xFu;
+                }
+                if (connectorDirection.Value == GVElectricConnectorDirection.Bottom)
+                {
+                    return (GetClockValue() >> 8) & 0xFu;
+                }
+                if (connectorDirection.Value == GVElectricConnectorDirection.Left)
+                {
+                    return (GetClockValue() >> 12) & 0xFu;
+                }
+                if (connectorDirection.Value == GVElectricConnectorDirection.In)
+                {
+                    return (GetClockValue() >> 16) & 0xFu;
+                }
+            }
+            return 0u;
+        }
+
+        public override bool Simulate()
+        {
+            double day = m_subsystemTimeOfDay.Day;
+            int num = (int)(((MathUtils.Ceiling(day * 4096.0) + 0.5) / 4096.0 - day) * 1200.0 / 0.0099999997764825821);
+            int circuitStep = MathUtils.Max(SubsystemGVElectricity.FrameStartCircuitStep + num, SubsystemGVElectricity.CircuitStep + 1);
+            SubsystemGVElectricity.QueueGVElectricElementForSimulation(this, circuitStep);
+            uint clockValue = GetClockValue();
+            if (clockValue != m_lastClockValue)
+            {
+                m_lastClockValue = clockValue;
+                return true;
+            }
+            return false;
+        }
+
+        public uint GetClockValue()
+        {
+            return (uint)(m_subsystemTimeOfDay.Day * 4096);
+        }
+    }
+}
