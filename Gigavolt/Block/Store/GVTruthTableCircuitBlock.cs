b50ec0820ee4738c6cad40c580ed8332e3416aef
销锋镝铸 <523084467@qq.com>
2023-01-29 22:35:21 +0800

    添加原版真值表

---
+++ Gigavolt/Block/Store/GVTruthTableCircuitBlock.cs
@@ -0,0 +1,35 @@
+namespace Game
+{
+    public class GVTruthTableCircuitBlock : RotateableMountedGVElectricElementBlock
+    {
+        public const int Index = 888;
+
+        public GVTruthTableCircuitBlock()
+            : base("Models/Gates", "TruthTableCircuit", 0.5f)
+        {
+        }
+
+        public override GVElectricElement CreateGVElectricElement(SubsystemGVElectricity subsystemGVElectricity, int value, int x, int y, int z)
+        {
+            return new TruthTableCircuitGVElectricElement(subsystemGVElectricity, new CellFace(x, y, z, GetFace(value)));
+        }
+
+        public override GVElectricConnectorType? GetConnectorType(SubsystemTerrain terrain, int value, int face, int connectorFace, int x, int y, int z)
+        {
+            int data = Terrain.ExtractData(value);
+            if (GetFace(value) == face)
+            {
+                GVElectricConnectorDirection? connectorDirection = SubsystemGVElectricity.GetConnectorDirection(GetFace(value), GetRotation(data), connectorFace);
+                if (connectorDirection == GVElectricConnectorDirection.Right || connectorDirection == GVElectricConnectorDirection.Left || connectorDirection == GVElectricConnectorDirection.Bottom || connectorDirection == GVElectricConnectorDirection.Top)
+                {
+                    return GVElectricConnectorType.Input;
+                }
+                if (connectorDirection == GVElectricConnectorDirection.In)
+                {
+                    return GVElectricConnectorType.Output;
+                }
+            }
+            return null;
+        }
+    }
+}
---
+++ Gigavolt/Block/Store/SubsystemGVTruthTableCircuitBlockBehavior.cs
@@ -0,0 +1,52 @@
+using Engine;
+
+namespace Game
+{
+    public class SubsystemGVTruthTableCircuitBlockBehavior : SubsystemEditableItemBehavior<GVTruthTableData>
+    {
+        public override int[] HandledBlocks => new int[1]
+        {
+            888
+        };
+
+        public SubsystemGVTruthTableCircuitBlockBehavior()
+            : base(888)
+        {
+        }
+
+        public override bool OnEditInventoryItem(IInventory inventory, int slotIndex, ComponentPlayer componentPlayer)
+        {
+            if (componentPlayer.DragHostWidget.IsDragInProgress) return false;
+            int value = inventory.GetSlotValue(slotIndex);
+            int count = inventory.GetSlotCount(slotIndex);
+            int id = Terrain.ExtractData(value);
+            GVTruthTableData truthTableData = GetItemData(id);
+            truthTableData = truthTableData != null ? (GVTruthTableData)truthTableData.Copy() : new GVTruthTableData();
+            DialogsManager.ShowDialog(componentPlayer.GuiWidget, new EditGVTruthTableDialog(truthTableData, delegate
+            {
+                int data = StoreItemDataAtUniqueId(truthTableData);
+                int value2 = Terrain.ReplaceData(value, data);
+                inventory.RemoveSlotItems(slotIndex, count);
+                inventory.AddSlotItems(slotIndex, value2, count);
+            }));
+            return true;
+        }
+
+        public override bool OnEditBlock(int x, int y, int z, int value, ComponentPlayer componentPlayer)
+        {
+            GVTruthTableData truthTableData = GetBlockData(new Point3(x, y, z)) ?? new GVTruthTableData();
+            DialogsManager.ShowDialog(componentPlayer.GuiWidget, new EditGVTruthTableDialog(truthTableData, delegate
+            {
+                SetBlockData(new Point3(x, y, z), truthTableData);
+                int face = ((GVTruthTableCircuitBlock)BlocksManager.Blocks[888]).GetFace(value);
+                SubsystemGVElectricity subsystemGVElectricity = SubsystemTerrain.Project.FindSubsystem<SubsystemGVElectricity>(throwOnError: true);
+                GVElectricElement GVElectricElement = subsystemGVElectricity.GetGVElectricElement(x, y, z, face);
+                if (GVElectricElement != null)
+                {
+                    subsystemGVElectricity.QueueGVElectricElementForSimulation(GVElectricElement, subsystemGVElectricity.CircuitStep + 1);
+                }
+            }));
+            return true;
+        }
+    }
+}
