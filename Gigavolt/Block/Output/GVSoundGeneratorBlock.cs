32cb723eb72f112a74dbd62a81e6e6b1b713bd63
销锋镝铸 <523084467@qq.com>
2023-01-29 23:10:13 +0800

    添加原版声音发生器

---
+++ Gigavolt/Block/Output/GVSoundGeneratorBlock.cs
@@ -0,0 +1,31 @@
+namespace Game
+{
+    public class GVSoundGeneratorBlock : RotateableMountedGVElectricElementBlock
+    {
+        public const int Index = 883;
+
+        public GVSoundGeneratorBlock()
+            : base("Models/Gates", "SoundGenerator", 0.5f)
+        {
+        }
+
+        public override GVElectricElement CreateGVElectricElement(SubsystemGVElectricity subsystemGVElectricity, int value, int x, int y, int z)
+        {
+            return new SoundGeneratorGVElectricElement(subsystemGVElectricity, new CellFace(x, y, z, GetFace(value)));
+        }
+
+        public override GVElectricConnectorType? GetConnectorType(SubsystemTerrain terrain, int value, int face, int connectorFace, int x, int y, int z)
+        {
+            int data = Terrain.ExtractData(value);
+            if (GetFace(value) == face)
+            {
+                GVElectricConnectorDirection? connectorDirection = SubsystemGVElectricity.GetConnectorDirection(GetFace(value), GetRotation(data), connectorFace);
+                if (connectorDirection == GVElectricConnectorDirection.Bottom || connectorDirection == GVElectricConnectorDirection.Top || connectorDirection == GVElectricConnectorDirection.Right || connectorDirection == GVElectricConnectorDirection.Left || connectorDirection == GVElectricConnectorDirection.In)
+                {
+                    return GVElectricConnectorType.Input;
+                }
+            }
+            return null;
+        }
+    }
+}
---
+++ Gigavolt/Block/Output/SoundGeneratorGVElectricElement.cs
@@ -0,0 +1,168 @@
+using Engine;
+
+namespace Game
+{
+    public class SoundGeneratorGVElectricElement : RotateableGVElectricElement
+    {
+        public SubsystemNoise m_subsystemNoise;
+
+        public SubsystemParticles m_subsystemParticles;
+
+        public SoundParticleSystem m_particleSystem;
+
+        public Random m_random = new Random();
+
+        public uint m_lastToneInput;
+
+        public double m_playAllowedTime;
+
+        public string[] m_tones = new string[16]
+        {
+            "",
+            "Bell",
+            "Organ",
+            "Ping",
+            "String",
+            "Trumpet",
+            "Voice",
+            "Piano",
+            "PianoLong",
+            "Drums",
+            "",
+            "",
+            "",
+            "",
+            "",
+            "Piano"
+        };
+
+        public int[] m_maxOctaves = new int[16]
+        {
+            0,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            6,
+            6,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            6
+        };
+
+        public string[] m_drums = new string[10]
+        {
+            "Snare",
+            "BassDrum",
+            "ClosedHiHat",
+            "PedalHiHat",
+            "OpenHiHat",
+            "LowTom",
+            "HighTom",
+            "CrashCymbal",
+            "RideCymbal",
+            "HandClap"
+        };
+
+        public SoundGeneratorGVElectricElement(SubsystemGVElectricity subsystemGVElectricity, CellFace cellFace)
+            : base(subsystemGVElectricity, cellFace)
+        {
+            m_subsystemNoise = subsystemGVElectricity.Project.FindSubsystem<SubsystemNoise>(throwOnError: true);
+            m_subsystemParticles = subsystemGVElectricity.Project.FindSubsystem<SubsystemParticles>(throwOnError: true);
+            Vector3 vector = CellFace.FaceToVector3(cellFace.Face);
+            Vector3 position = new Vector3(cellFace.Point) + new Vector3(0.5f) - 0.2f * vector;
+            m_particleSystem = new SoundParticleSystem(subsystemGVElectricity.SubsystemTerrain, position, vector);
+        }
+
+        public override bool Simulate()
+        {
+            uint num = 0;
+            uint num2 = 15;
+            uint num3 = 2;
+            uint num4 = 0;
+            int rotation = Rotation;
+            foreach (GVElectricConnection connection in Connections)
+            {
+                if (connection.ConnectorType != GVElectricConnectorType.Output && connection.NeighborConnectorType != 0)
+                {
+                    GVElectricConnectorDirection? connectorDirection = SubsystemGVElectricity.GetConnectorDirection(CellFaces[0].Face, rotation, connection.ConnectorFace);
+                    if (connectorDirection.HasValue)
+                    {
+                        if (connectorDirection.Value == GVElectricConnectorDirection.In || connectorDirection.Value == GVElectricConnectorDirection.Bottom)
+                        {
+                            num4 = connection.NeighborGVElectricElement.GetOutputVoltage(connection.NeighborConnectorFace) & 0xfu;
+                        }
+                        else if (connectorDirection.Value == GVElectricConnectorDirection.Left)
+                        {
+                            num = connection.NeighborGVElectricElement.GetOutputVoltage(connection.NeighborConnectorFace) & 0xfu;
+                        }
+                        else if (connectorDirection.Value == GVElectricConnectorDirection.Right)
+                        {
+                            num3 = connection.NeighborGVElectricElement.GetOutputVoltage(connection.NeighborConnectorFace) & 0xfu;
+                        }
+                        else if (connectorDirection.Value == GVElectricConnectorDirection.Top)
+                        {
+                            num2 = connection.NeighborGVElectricElement.GetOutputVoltage(connection.NeighborConnectorFace) & 0xfu;
+                        }
+                    }
+                }
+            }
+            if (m_lastToneInput == 0 && num4 != 0 && num != 15 && SubsystemGVElectricity.SubsystemTime.GameTime >= m_playAllowedTime)
+            {
+                m_playAllowedTime = SubsystemGVElectricity.SubsystemTime.GameTime + 0.079999998211860657;
+                string text = m_tones[num4];
+                float num5 = 0f;
+                string text2 = null;
+                if (text == "Drums")
+                {
+                    num5 = 1f;
+                    if (num >= 0 && num < m_drums.Length)
+                    {
+                        text2 = $"Audio/SoundGenerator/Drums{m_drums[num]}";
+                    }
+                }
+                else if (!string.IsNullOrEmpty(text))
+                {
+                    float num6 = 130.8125f * MathUtils.Pow(1.05946314f, num + 12 * num3);
+                    int num7 = 0;
+                    for (int i = 4; i <= m_maxOctaves[num4]; i++)
+                    {
+                        float num8 = num6 / (523.25f * MathUtils.Pow(2f, i - 5));
+                        if (num7 == 0 || (num8 >= 0.5f && num8 < num5))
+                        {
+                            num7 = i;
+                            num5 = num8;
+                        }
+                    }
+                    text2 = $"Audio/SoundGenerator/{text}C{num7}";
+                }
+                if (num5 != 0f && !string.IsNullOrEmpty(text2))
+                {
+                    CellFace cellFace = CellFaces[0];
+                    var position = new Vector3(cellFace.X, cellFace.Y, cellFace.Z);
+                    float volume = num2 / 15f;
+                    float pitch = MathUtils.Clamp(MathUtils.Log(num5) / MathUtils.Log(2f), -1f, 1f);
+                    float minDistance = 0.5f + 5f * num2 / 15f;
+                    SubsystemGVElectricity.SubsystemAudio.PlaySound(text2, volume, pitch, position, minDistance, autoDelay: true);
+                    float loudness = (num2 < 8) ? 0.25f : 0.5f;
+                    float range = MathUtils.Lerp(2f, 20f, num2 / 15f);
+                    m_subsystemNoise.MakeNoise(position, loudness, range);
+                    if (m_particleSystem.SubsystemParticles == null)
+                    {
+                        m_subsystemParticles.AddParticleSystem(m_particleSystem);
+                    }
+                    var hsv = new Vector3(22.5f * num + m_random.Float(0f, 22f), 0.5f + num2 / 30f, 1f);
+                    m_particleSystem.AddNote(new Color(Color.HsvToRgb(hsv)));
+                }
+            }
+            m_lastToneInput = num4;
+            return false;
+        }
+    }
+}
