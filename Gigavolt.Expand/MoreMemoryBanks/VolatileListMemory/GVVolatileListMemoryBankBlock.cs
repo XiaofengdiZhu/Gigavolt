365b8f0c9bd331959f1ba9acf7851e7b9544c8eb
销锋镝铸 <523084467@qq.com>
2023-09-20 00:53:04 +0800

    为扩展里的M板新增材质

--- Gigavolt.Expand/MoreMemoryBanks/VolatileListMemory/GVVolatileListMemoryBankBlock.cs
+++ Gigavolt.Expand/MoreMemoryBanks/VolatileListMemory/GVVolatileListMemoryBankBlock.cs
@@ -1,8 +1,54 @@
+using Engine;
+using Engine.Graphics;
+
 namespace Game {
     public class GVVolatileListMemoryBankBlock : RotateableMountedGVElectricElementBlock {
         public const int Index = 873;
+        public Texture2D m_texture;
 
-        public GVVolatileListMemoryBankBlock() : base("Models/Gates", "MemoryBank", 0.875f) { }
+        public GVVolatileListMemoryBankBlock() : base("Models/GVMemoryBank", "MemoryBank", 0.875f) { }
+
+        public override void Initialize() {
+            base.Initialize();
+            m_texture = ContentManager.Get<Texture2D>("Textures/GVVolatileListMemoryBankBlock");
+        }
+
+        public override void DrawBlock(PrimitivesRenderer3D primitivesRenderer, int value, Color color, float size, ref Matrix matrix, DrawBlockEnvironmentData environmentData) {
+            BlocksManager.DrawMeshBlock(
+                primitivesRenderer,
+                m_standaloneBlockMesh,
+                m_texture,
+                color,
+                2f * size,
+                ref matrix,
+                environmentData
+            );
+        }
+
+        public override void GenerateTerrainVertices(BlockGeometryGenerator generator, TerrainGeometry geometry, int value, int x, int y, int z) {
+            int num = Terrain.ExtractData(value) & 0x1F;
+            generator.GenerateMeshVertices(
+                this,
+                x,
+                y,
+                z,
+                m_blockMeshes[num],
+                Color.White,
+                null,
+                geometry.GetGeometry(m_texture).SubsetOpaque
+            );
+            GenerateGVWireVertices(
+                generator,
+                value,
+                x,
+                y,
+                z,
+                GetFace(value),
+                m_centerBoxSize,
+                Vector2.Zero,
+                geometry.SubsetOpaque
+            );
+        }
 
         public override GVElectricElement CreateGVElectricElement(SubsystemGVElectricity subsystemGVElectricity, int value, int x, int y, int z) => new VolatileListMemoryBankGVElectricElement(subsystemGVElectricity, new CellFace(x, y, z, GetFace(value)));
 
