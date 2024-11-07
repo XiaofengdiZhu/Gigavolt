2c40827e684fabf426493333cc4a076c2e6d119c
XiaofengdiZhu <523084467@qq.com>
2024-11-07 17:49:14 +0800

    微调项目文件

--- Gigavolt.Expand/Transportation/GuidedDispenser/GVGuidedDispenserBlock.cs
+++ Gigavolt.Expand/Transportation/GuidedDispenser/GVGuidedDispenserBlock.cs
@@ -99,5 +99,10 @@ namespace Game {
         public GVElectricConnectorType? GetGVConnectorType(SubsystemGVSubterrain subsystem, int value, int face, int connectorFace, int x, int y, int z, Terrain terrain) => GVElectricConnectorType.Input;
 
         public int GetConnectionMask(int value) => int.MaxValue;
+
+        public override bool IsMovableByPiston(int value, int pistonFace, int y, out bool isEnd) {
+            isEnd = false;
+            return false;
+        }
     }
 }
